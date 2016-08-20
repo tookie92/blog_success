635978bb519b7cb1a33b4d1382a0b75dc99e7e26
joseph <josephikinda@gmail.com>
2016-08-20 02:31:56 +0000

    Partie du menu

--- public/js/init.js
+++ public/js/init.js
@@ -3,6 +3,10 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $('#tohash').on('click', function(){
+  		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+  		return false;
+  	});
   }); // end of document ready
+
 })(jQuery); // end of jQuery name space
