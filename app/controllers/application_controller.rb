33e2742659b72dafbc754382e1ae044bd67f5609
joseph <josephikinda@gmail.com>
2016-08-21 12:33:16 +0000

    login done now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+before_action :authenticate_user!
   # pour ajouter un champ dans le formulaire user
    before_action :configure_permitted_parameters, if: :devise_controller?
 
 	protected
 		def configure_permitted_parameters
 		  devise_parameter_sanitizer.permit(:sign_up, keys: [:nom, :prenom, :email, :password, :profil,
-       :profil_name, :profil_uid ])
+       :profil_name, :profil_uid, :cover, :cover_uid, :cover_name ])
 		  devise_parameter_sanitizer.permit(:account_update, keys: [:nom, :prenom, :profil, :citation, :cover,
         :email, :password, :profil_uid, :profil_name, :cover_uid, :cover_name ])
 		end
